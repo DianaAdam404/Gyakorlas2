4ce17f00847574b9691fc403f89c45699388aba4
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-17 08:36:14 +0200

    1. Feladat

--- FilmApp/Program.cs
+++ FilmApp/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using FilmApp;
+Film elsofilm = new Film();
+elsofilm.Cim = "A Gyuruk ura";
+elsofilm.HosszPercben = 178;
+
+Film masodikfilm = new Film();
+Console.Write("Adja meg a  masodik film cimet: ");
+masodikfilm.Cim = Console.ReadLine();
+Console.Write("Adja meg a  masodik film cimet: ");
+masodikfilm.HosszPercben = int.Parse(Console.ReadLine());
+Console.WriteLine($"Az elso film cime: {elsofilm.Cim}, hossza percben: {elsofilm.HosszPercben}");
+Console.WriteLine($"A masodik film cime: {masodikfilm.Cim}, hossza percben: {masodikfilm.HosszPercben}");
