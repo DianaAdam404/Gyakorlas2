4d9ad5743de5fb58b4fa13b25326290ee0ee5187
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-17 09:45:11 +0200

    Kave

--- KavezogepApp/Kavezogep.cs
+++ KavezogepApp/Kavezogep.cs
@@ -6,5 +6,28 @@ namespace KavezogepApp
 {
     internal class Kavezogep
     {
+        private int vizKeszletMl;
+        public void VizTolt(int ml)
+        {
+            if (ml < 0)
+            {
+                vizKeszletMl = 0;
+                Console.WriteLine("Hiba! Negativ ertek!");
+            }
+            else vizKeszletMl += ml;
+        }
+        public void KaveFozes()
+        {
+            if (vizKeszletMl >= 150)
+            {
+                vizKeszletMl -= 150;
+                Console.WriteLine("Kave");
+
+            }
+            else
+            {
+                Console.WriteLine("No kave");
+            }
+        }
     }
 }
