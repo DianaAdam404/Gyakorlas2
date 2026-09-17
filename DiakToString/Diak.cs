5824e7ad20814e3bf4e354cb78d8d3f04e9093e6
DianaAdam404 <adam.diana.2022d@telekimezotur.hu>
2026-09-17 09:34:59 +0200

    TOSTRING

---
+++ DiakToString/Diak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiakToString
+{
+    internal class Diak
+    {
+        public string Nev { get; set; }
+        public double Atlag { get; set; }
+        public Diak()
+        {
+            Nev = "Kiss Peter";
+            Atlag = 4.5;
+        }
+        public Diak (string kapottnev, double kapottatlag)
+        {
+            Nev = kapottnev;
+            Atlag = kapottatlag;
+        }
+        public Diak(string csaknev)
+        {
+            Nev = csaknev;
+            Atlag=2.5;
+        }
+
+        public override string ToString()
+        {
+            return $"Név: {Nev}, Atlag: {Atlag}";
+        }
+    }
+}
--- DiakToString/Program.cs
+++ DiakToString/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DiakToString;
+
+Diak diak1 = new Diak();
+Diak diak2 = new Diak("Nagy Jonas");
+Diak diak3 = new Diak("Kovacs Anna", 3.8);
+
+Console.WriteLine(diak1);
+Console.WriteLine(diak2);
+Console.WriteLine(diak3);
